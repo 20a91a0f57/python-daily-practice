ca20a85b5e870c21d1dabaaa613b34160398f147
20a91a0f57 <84563253+20a91a0f57@users.noreply.github.com>
2026-02-19 22:30:34 +0530

    Update day1_basics.py

--- day1_basics.py
+++ day1_basics.py
@@ -1,1 +1,17 @@
-print("Hello!")
+"""DAY-1 — Python Introduction + Setup + Basics"""
+"""In Command prompt"""
+C:\Users\sree>python --version
+Python 3.14.3
+C:\Users\sree>python
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>>
+>>> print("Hello!")
+Hello!  
+>>> """variable"""
+'variable'
+>>> name = "Madhuri"
+>>> age = 22
+>>> percentage = 85.5
+>>> print(name, age, percentage)
+Madhuri 22 85.5
